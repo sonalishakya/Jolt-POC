ab9186242278cf501279b9525d560ad06ec03a21
Sonali Shakya <49765278+sonalishakya@users.noreply.github.com>
2024-09-30 14:36:16 +0530

    Adding maven project to git

---
+++ Jolt-POC/src/main/java/com/ondc/Jolt_POC/JoltPocApplication.java
@@ -0,0 +1,13 @@
+package com.ondc.Jolt_POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoltPocApplication.class, args);
+	}
+
+}
---
+++ Jolt-POC/src/test/java/com/ondc/Jolt_POC/JoltPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondc.Jolt_POC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoltPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
